ef161c8649ad02a6ff468fcb79df8d397d1a1bd1
Julien Bisconti <julienbi@unity3d.com>
2019-04-23 16:44:27 +0100

    &&

--- ReversiProj/botfunction.c
+++ ReversiProj/botfunction.c
@@ -7,6 +7,7 @@ int contaPontos (ESTADO *e,int x, int y) {
     ESTADO *aux = malloc(sizeof(struct estado));
     copyEstado(e, aux);
     joga(aux, x, y);
+    int flag = 1;
 //    poepeca(aux, x, y);
     // ----------------------------
 
@@ -16,9 +17,9 @@ int contaPontos (ESTADO *e,int x, int y) {
         for (k = 0; k < 8; k++) {
             if (aux->grelha[i][k] == aux->peca) {
                 if ((i == 0 || i == 7) && (k == 0 || k == 7)) soma += 10000;
-                else if ((i == 1 && k == 1) || (i == 1 && k == 6) || (i == 6 && k == 1) || (i == 6 && k == 6)) soma = soma - 5000;
+                else if ((i == 1 && k == 1) || (i == 1 && k == 6) || (i == 6 && k == 1) || (i == 6 && k == 6)) soma -= 5000;
                 else if ((i == 1 && k == 0) || (i == 0 && k == 1) || (i == 0 && k == 6) || (i == 1 && k == 7) || (i == 6 && k == 0) || (i == 7 && k == 1) || (i == 6 && k == 7) || (i == 7 && k == 6))
-                    soma = soma - 3000;
+                    soma -= 3000;
                 else if ((i == 0 && k == 2) || (i == 0 && k == 5) || (i == 7 && k == 2) || (i == 7 && k == 5) ||
                          (i == 2 && k == 0) || (i == 2 && k == 7) || (i == 5 && k == 0) || (i == 5 && k == 7))
                     soma += 1000;
@@ -27,10 +28,10 @@ int contaPontos (ESTADO *e,int x, int y) {
                     soma += 800;
                 else if ((i == 1 && k == 3) || (i == 1 && k == 4) || (i == 6 && k == 3) || (i == 6 && k == 4) ||
                          (i == 3 && k == 1) || (i == 3 && k == 6) || (i == 4 && k == 1) || (i == 4 && k == 6))
-                    soma = soma - 500;
+                    soma -= 500;
                 else if ((i == 1 && k == 2) || (i == 2 && k == 1) || (i == 1 && k == 5) || (i == 2 && k == 6) ||
                          (i == 5 && k == 1) || (i == 6 && k == 2) || (i == 5 && k == 6) || (i == 6 && k == 5))
-                    soma = soma -450;
+                    soma -= 450;
                 else if ((i == 2 && k == 2) || (i == 2 && k == 5) || (i == 5 && k == 2) || (i == 5 && k == 5))
                     soma += 30;
                 else if ((i == 2 && k == 3) || (i == 2 && k == 4) || (i == 5 && k == 3) || (i == 5 && k == 4) ||
@@ -40,6 +41,30 @@ int contaPontos (ESTADO *e,int x, int y) {
             }
         }
     }
+    for (i=0,k=0;k<8;k++) {
+        if (aux->grelha[i][k] != aux->peca) flag = 0;
+    }
+    if (flag == 1) soma+= 100000;
+    flag = 1;
+
+    for (i=7,k=0;k<8;k++) {
+        if (aux->grelha[i][k] != aux->peca) flag = 0;
+    }
+    if (flag == 1) soma+= 100000;
+    flag = 1;
+
+    for (i=0,k=0;i<8;i++) {
+        if (aux->grelha[i][k] != aux->peca) flag = 0;
+    }
+    if (flag == 1) soma+= 100000;
+    flag = 1;
+
+    for (i=0,k=7;i<8;i++) {
+        if (aux->grelha[i][k] != aux->peca) flag = 0;
+    }
+    if (flag == 1) soma+= 100000;
+
+
     free(aux);
     return soma;
 }

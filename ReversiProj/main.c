c334e5f265978d605f4189c283389eeaff6a2c79
56Aang <48497893+56aang@users.noreply.github.com>
2019-05-08 11:22:45 +0100

    alpha beta teste

--- ReversiProj/main.c
+++ ReversiProj/main.c
@@ -4,14 +4,15 @@
 #include "ctype.h"
 #include "botfunction.h"
 #include "math.h"
+
 int main() {
-    char c1,c2;
+    char c1, c2;
     char linha[50];
     int x, y;
-    int contaX=2,contaO=2;
+    int contaX = 2, contaO = 2;
     int resultado = 0;
     int state = 0;
-    int line,col;
+    int line, col;
     int nivel = 0;
     VALOR piece = VAZIA; // auxiliar p/ bot
     ESTADO *e = malloc(sizeof(struct estado));
@@ -23,9 +24,8 @@ int main() {
 
         fgets(linha, 50, stdin);
         switch (toupper(linha[0])) {
-            case 'T' :
-            {
-                sscanf(linha,"%c %c %d",&c1,&c2,&nivel);
+            case 'T' : {
+                sscanf(linha, "%c %c %d", &c1, &c2, &nivel);
                 if (nivel < 1);
                 else {
                     novoEstado(e, linha);
@@ -37,26 +37,26 @@ int main() {
                 }
                 break;
             }
-            case '?' :
-            {
-                interface ();
+            case '?' : {
+                interface();
                 break;
             }
-            case 'N' :
-            { // cria novo jogo
+            case 'N' : { // cria novo jogo
                 ESTADO *aux = e;
                 while (aux->ant != NULL) {
                     aux = aux->ant;
                     free(e);
                     e = aux;
                 }
-                novoEstado(e,linha);
+                novoEstado(e, linha);
                 state = 1;
                 resultado = 0;
+                printa(*e, &contaX, &contaO);
+                printf("\n#X = %d   #O = %d\n", contaX, contaO);
                 break;
             }
             case 'A' : {
-                sscanf(linha,"%c %c %d",&c1,&c2,&nivel);
+                sscanf(linha, "%c %c %d", &c1, &c2, &nivel);
                 if (nivel < 1 || (toupper(c2) != 'X' && toupper(c2) != 'O'));
                 else {
                     ESTADO *aux = e;
@@ -72,13 +72,14 @@ int main() {
                     e->nivel = nivel;
                     if (toupper(c2) == 'X') {
                         piece = VALOR_X;
-                        maxplay(e, &line, &col, nivel*2+1, nivel*2+1,9999,-9999);
+                        maxplay(e, &line, &col, nivel * 2 + 1, nivel * 2 + 1, 9999, -9999);
                         joga(e, line, col);
                         printa(*e, &contaX, &contaO);
                         printf("\n#X = %d   #O = %d\n", contaX, contaO);
                         trocapeca(e);
-                    }
-                    else piece = VALOR_O;
+                    } else piece = VALOR_O;
+                    printa(*e, &contaX, &contaO);
+                    printf("\n#X = %d   #O = %d\n", contaX, contaO);
                 }
                 break;
             }
@@ -94,7 +95,7 @@ int main() {
                     if (e->modo == 0 || e->modo == 1) {
                         if (e->peca != piece) {
                             sscanf(linha, "%c %d %d", &c1, &x, &y);
-                            if (e->validade[x-1][y-1]) {
+                            if (e->validade[x - 1][y - 1]) {
 
                                 //avançar o estado
                                 ESTADO *aux = malloc(sizeof(struct estado));
@@ -110,8 +111,7 @@ int main() {
                                 else if (playerPlayable(e)) {
                                     if (e->peca == VALOR_X) printf("Jogador O sem jogadas válidas!\n");
                                     else printf("Jogador X sem jogadas válidas!\n");
-                                }
-                                else resultado = gameOver(e, &contaX, &contaO);
+                                } else resultado = gameOver(e, &contaX, &contaO);
 
 
                             } else printf("Jogada inválida. Introduza uma jgoada válida.\n");
@@ -123,7 +123,8 @@ int main() {
                                 // as funções do bot vão aqui
 
                                 printf("%d\n",
-                                       maxplay(e, &line, &col, nivel*2+1, nivel*2+1,9999,-9999)); // a line e a col ficam com as posições onde deve jogar
+                                       maxplay(e, &line, &col, nivel * 2 + 1, nivel * 2 + 1, 9999,
+                                               -9999)); // a line e a col ficam com as posições onde deve jogar
 
                                 joga(e, line, col);
                                 printa(*e, &contaX, &contaO);
@@ -140,20 +141,7 @@ int main() {
                         }
                         // ---------------------------------------------------------------
 
-                    }
-
-
-
-
-
-
-
-
-
-
-
-
-                    else if (e->modo == 3) {
+                    } else if (e->modo == 3) {
                         do {
                             if (!playerPlayable(e) && oppPlayable(e)) {
                                 if (e->peca == VALOR_X) printf("Jogador X sem jogadas válidas!\n");
@@ -163,10 +151,11 @@ int main() {
                                 do {
                                     // as funções do bot vão aqui
                                     printf("%d\n",
-                                           maxplay(e, &line, &col,nivel*2+1,nivel*2+1,
+                                           maxplay(e, &line, &col, nivel * 2 + 1, nivel * 2 + 1,
 
 
-                                                   9999,-9999)); // a line e a col ficam com as posições onde deve jogar
+                                                   9999,
+                                                   -9999)); // a line e a col ficam com as posições onde deve jogar
 
                                     joga(e, line, col);
                                     printa(*e, &contaX, &contaO);
@@ -178,12 +167,10 @@ int main() {
 
                                 // -----------------
                                 // se o jogador não puder jogar
-
                             }
-                        }while (resultado == 0);
+                        } while (resultado == 0);
                     }
-                }
-                else printf("Jogo ainda não criado // N <peça>\n");
+                } else printf("Jogo ainda não criado // N <peça>\n");
                 break;
             }
 
@@ -209,11 +196,9 @@ int main() {
                         free(aux);
                         resultado = 0;
                     }
-                }
-                else printf("Jogo ainda não criado // N <peça>\n");
+                } else printf("Jogo ainda não criado // N <peça>\n");
                 break;
             }
-
             case 'H' : { // coloca pontos de interrogação em locais sugeridos para jogar
                 if (state != 1) printf("Jogo ainda não criado // N <peça>\n");
                 else if (playerPlayable(e)) giveHint(e);
@@ -237,8 +222,7 @@ int main() {
                     verificajogada2(e);
                     nivel = e->nivel;
                     resultado = gameOver(e, &contaX, &contaO);
-                }
-                else {
+                } else {
                     state = 0;
                     e->peca = VAZIA;
                     resultado = 0;
